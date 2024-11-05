ba463dfbfe973ba58b7aae030073732d331715aa
Yuto Terada <lugongdao07@gmail.com>
2024-11-05 21:49:08 +0900

    html langをjaに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="ja" className="dark">
       <body
         className={`${notoSansJP.variable} antialiased`}
       >
