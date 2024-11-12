596653f4f747125716676e591592f4d1349a5a71
Yuto Terada <lugongdao07@gmail.com>
2024-11-13 08:33:00 +0900

    fix: missing prop

--- src/app/[lang]/(auth-flow)/account/sign-up/page.tsx
+++ src/app/[lang]/(auth-flow)/account/sign-up/page.tsx
@@ -2,13 +2,19 @@ import { auth } from "@/auth";
 import Form from "./form";
 import { localRedirect } from "@/lib/localRedirect";
 
-export default async function Page({ searchParams: { returnUrl, email } }: { searchParams: { returnUrl?: string, email?: string } }) {
+export default async function Page({
+  searchParams: { returnUrl, email },
+  params: { lang }
+}: {
+  searchParams: { returnUrl?: string, email?: string },
+  params: { lang: string }
+}) {
   const session = await auth();
   if (session) {
     localRedirect(returnUrl || "/");
   }
 
   return (
-    <Form returnUrl={returnUrl} email={email} />
+    <Form returnUrl={returnUrl} email={email} lang={lang} />
   )
 }
