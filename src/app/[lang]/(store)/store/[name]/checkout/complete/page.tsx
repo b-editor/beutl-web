e24a9681f38407b71641b954a01bc20b4a2ff107
Yuto Terada <lugongdao07@gmail.com>
2024-11-24 02:36:06 +0900

    d

--- src/app/[lang]/(store)/store/[name]/checkout/complete/page.tsx
+++ src/app/[lang]/(store)/store/[name]/checkout/complete/page.tsx
@@ -21,7 +21,7 @@ export default async function Page({
   return (
     <div className="max-w-5xl mx-auto py-10 lg:py-6 px-2 bg-card lg:rounded-lg border text-card-foreground lg:my-4">
       <div className="max-sm:relative flex max-md:flex-col gap-2">
-        <div className="md:flex-1 mx-3">
+        <div className="md:flex-1 mx-3 min-w-0">
           <PackageDetails
             pkg={pkg}
             price={intent.amount}
