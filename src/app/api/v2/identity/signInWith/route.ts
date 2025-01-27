01c0779a6dba9126f93e107e6f6cc2db8ae88db7
Yuto Terada <lugongdao07@gmail.com>
2025-01-28 00:47:38 +0900

    refactor: Improve sign-in redirect logic by using URL constructor for better URL handling

--- src/app/api/v2/identity/signInWith/route.ts
+++ src/app/api/v2/identity/signInWith/route.ts
@@ -1,8 +1,10 @@
 import { type NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const provider = encodeURIComponent(req.nextUrl.searchParams.get("provider") as string);
-  const returnUrl = encodeURIComponent(req.nextUrl.searchParams.get("returnUrl") as string);
-
-  return NextResponse.redirect(`/account/native-auth/sign-in-with?provider=${provider}&returnUrl=${returnUrl}`);
+export function GET(req: NextRequest) {
+  const provider = req.nextUrl.searchParams.get("provider") as string;
+  const returnUrl = req.nextUrl.searchParams.get("returnUrl") as string;
+  const url = new URL("/account/native-auth/sign-in-with", req.nextUrl.origin);
+  url.searchParams.set("provider", provider);
+  url.searchParams.set("returnUrl", returnUrl);
+  return NextResponse.redirect(url);
 }
