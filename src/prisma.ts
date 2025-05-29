39867aee377654a5c15d9ca414b43353f3dc0f8f
Yuto Terada <lugongdao07@gmail.com>
2025-05-29 14:55:47 +0900

    fix: PrismaClientの初期化を修正し、データソースURLをCloudflareコンテキストから取得

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma || new PrismaClient({
+    datasourceUrl: getCloudflareContext().env.BEUTL_DATABASE_HYPERDRIVE.connectionString || process.env.DATABASE_URL,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
