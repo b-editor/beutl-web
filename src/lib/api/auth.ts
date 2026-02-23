f035917ffe33b0aae61d9f5ec7086f94d6c02ae6
Yuto Terada <lugongdao07@gmail.com>
2026-02-23 23:56:07 +0900

    Revert "fix: specify algorithm for jwtVerify in getUserId function"
    This reverts commit 28c5cc9627931dbde905badab4364aaa1667fe82.

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -7,7 +7,7 @@ export async function getUserId(c: Context) {
   if (!header) return null;
   if (!header.startsWith("Bearer ")) return null;
   const token = header.split(" ")[1];
-  const payload = await jwtVerify(token, process.env.JWT_SECRET as string, "HS256");
+  const payload = await jwtVerify(token, process.env.JWT_SECRET as string);
   return payload[
     "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
   ] as string;
