c4b537c7a899012b556ff4c67fd4b08f71f8d380
Yuto Terada <lugongdao07@gmail.com>
2026-03-15 16:23:01 +0900

    feat: refactor addToLibrary function and remove unused page component

--- src/app/[lang]/(store)/store/[name]/actions.tsx
+++ src/app/[lang]/(store)/store/[name]/actions.tsx
@@ -2,24 +2,64 @@
 
 import { getTranslation } from "@/app/i18n/server";
 import { addAuditLog, auditLogActions } from "@/lib/audit-log";
-import { authenticated } from "@/lib/auth-guard";
+import { authenticated, authOrSignIn } from "@/lib/auth-guard";
+import { existsUserPaymentHistory } from "@/lib/db/user-payment-history";
 import { getLanguage } from "@/lib/lang-utils";
 import { getDbAsync } from "@/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function addToLibrary(packageId: string) {
+  const session = await authOrSignIn();
   const lang = await getLanguage();
   const db = await getDbAsync();
-  const { name } = await db.package.findFirstOrThrow({
+
+  const pkg = await db.package.findFirst({
     where: {
       id: packageId,
+      published: true,
     },
     select: {
+      id: true,
       name: true,
+      packagePricing: true,
     },
   });
-  redirect(`/${lang}/store/${name}/get`);
+  if (!pkg) {
+    return { success: false, message: "Package not found" };
+  }
+
+  if (pkg.packagePricing.length > 0) {
+    // すでに支払いをしている場合のみ、支払わずにuserPackageを作成する
+    const record = await existsUserPaymentHistory({
+      userId: session.user.id,
+      packageId: pkg.id,
+    });
+    if (!record) {
+      redirect(`/${lang}/store/${pkg.name}/checkout`);
+    }
+  }
+
+  if (
+    !(await db.userPackage.findFirst({
+      where: { userId: session.user.id, packageId: pkg.id },
+    }))
+  ) {
+    await db.userPackage.create({
+      data: {
+        userId: session.user.id,
+        packageId: pkg.id,
+      },
+    });
+    await addAuditLog({
+      userId: session.user.id,
+      action: auditLogActions.store.addToLibrary,
+      details: `packageId: ${pkg.id}`,
+    });
+  }
+
+  revalidatePath(`/store/${pkg.name}`);
+  redirect(`/${lang}/store/${pkg.name}?message=PleaseOpenDesktopApp`);
 }
 
 export async function removeFromLibrary(packageId: string) {
